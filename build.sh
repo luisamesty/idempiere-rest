3962d0be22c1cbeaed0587e6a95ae5cbebdc8a98
luisamesty <luisamesty@gmail.com>
2023-12-28 08:27:00 +0100

    amerpsoft-release-10 for idempiere-rest

---
+++ build.sh
@@ -0,0 +1,6 @@
+# Build using idempiere 8.2 maven repo
+# SHOULD BE 
+# mvn -Dmaven.repo.local=$HOME/.m2/repository_10_OK clean install
+# BUT 
+mvn -Dmaven.repo.local=$HOME/.m2/repository_82_OK instal
+
